0f46abfee8afe4dc2ff65fffd47222e2c3e15848
Chetan Sharma <139779171+ChetanSharma200@users.noreply.github.com>
2025-12-22 10:41:42 +0530

    Add subarraysDivByK method for subarray sums
    Implement a solution to count subarrays with sums divisible by k using a hashmap.

--- Array/Prefix +Hashing/subarray_sums_divisible_by_k.java
+++ Array/Prefix +Hashing/subarray_sums_divisible_by_k.java
@@ -1,1 +1,19 @@
 
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        HashMap<Integer ,Integer> hm =  new HashMap<>();
+        int prev = 0 ; 
+        int ans = 0 ;
+        hm.put(0,1);
+
+        for(int i =0;i<nums.length;i++){
+            prev+=nums[i];
+
+            if(hm.containsKey(((prev%k)%k +k)%k)){
+                ans+=hm.get(((prev%k)%k +k)%k);
+            }
+            hm.put(((prev%k)%k +k)%k, hm.getOrDefault(((prev%k)%k +k)%k , 0) +1);
+        }
+        return ans;
+    }
+}
