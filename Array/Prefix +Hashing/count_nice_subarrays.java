19e211580f4bfbd4d9ce19ad5017ef003ad036e2
Chetan Sharma <139779171+ChetanSharma200@users.noreply.github.com>
2025-12-22 10:40:23 +0530

    Update count_nice_subarrays.java

--- Array/Prefix +Hashing/count_nice_subarrays.java
+++ Array/Prefix +Hashing/count_nice_subarrays.java
@@ -1,1 +1,18 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+          HashMap<Integer ,Integer> hm =  new HashMap<>();
+        int prev = 0 ; 
+        int ans = 0 ;
+        hm.put(0,1);
+
+        for(int i =0;i<nums.length;i++){
+            prev+=(nums[i] %2==0)?0:1;
+            if(hm.containsKey(prev - k)){
+                ans+=hm.get(prev - k);
+            }
+            hm.put(prev, hm.getOrDefault(prev, 0) +1);
+        }
+        return ans;
+    }
+}
 
