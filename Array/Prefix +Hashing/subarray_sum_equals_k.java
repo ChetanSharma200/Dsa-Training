7ce1561e17a1bef664b3a51f28b598410679f077
Chetan Sharma <139779171+ChetanSharma200@users.noreply.github.com>
2025-12-22 10:40:57 +0530

    Add solution for subarray sum equals k
    Implement subarray sum equals k using prefix sums.

--- Array/Prefix +Hashing/subarray_sum_equals_k.java
+++ Array/Prefix +Hashing/subarray_sum_equals_k.java
@@ -1,1 +1,20 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        int prefixSum=0;
+        int ans=0;
 
+        for(int i=0;i<nums.length;i++){
+             prefixSum+=nums[i];
+             if(prefixSum==k)ans++;
+
+             int rem=prefixSum-k;
+             if(hs.containsKey(rem)){
+                ans+=hs.get(rem);
+             }
+             
+            hs.put(prefixSum, hs.getOrDefault(prefixSum, 0)+1);
+        }
+        return ans;
+    }
+}
